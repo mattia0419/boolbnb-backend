157d8d425275db01c183f315e1a01e536d5594b7
FrancescoPepi <francescopepi16@gmail.com>
2023-11-22 19:19:45 +0100

    feat card incex

--- resources/views/admin/apartments/index.blade.php
+++ resources/views/admin/apartments/index.blade.php
@@ -1,25 +1,53 @@
 @extends('layouts.app')
 
 @section('content')
+    <div class="container mt-5">
+        <h1 class="mb-4">Apartments - {{ $user->first_name }}</h1>
+        <div class="row row-cols-4">
 
-<div class="container mt-5">
-    <h1 class="mb-4">Apartments - {{$user->email}}</h1>
-    <div class="row row-cols-4">
+            @foreach ($apartments as $apartment)
+                <div class="col">
+                    <div class="card h-100 text-center">
+                        <div class="card-image">
+                            <img src="{{ $apartment->cover_img }}" class="img-fluid" alt="...">
+                        </div>
+                        <div class="card-header">
+                            <h5 class="card-title">
+                                {{ $apartment->title }}
+                            </h5>
+                        </div>
+                        <div class="card-body pb-0 text-start">
+                            <p>
+                                @if ($apartment->address)
+                                    {{ $apartment->address }}
+                                @else
+                                    -
+                                @endif
+                            </p>
+                            <p>
+                                @if ($apartment->square_meters)
+                                    {{ $apartment->square_meters }}
+                                @else
+                                    -
+                                @endif
+                            </p>
+                            <p>
+                                @if ($apartment->price)
+                                    {{ $apartment->price }}
+                                @else
+                                    -
+                                @endif
+                            </p>
+                        </div>
+                        <div class="card-footer">
+                            <a href="#" class="btn col-4 btn-primary">EDIT</a>
+                            <a href="#" class="btn col-4 btn-primary">DETAILS</a>
 
-        @foreach($apartments as $apartment)
-        <div class="col">
-            <div class="card">
-                <div class="card-header">
-                    {{ $apartment->title}}
+                        </div>
+                    </div>
                 </div>
-                <div class="card-body"></div>
-                <div class="card-footer"></div>
-            </div>
+            @endforeach
         </div>
-        @endforeach
+
     </div>
-
-</div>
-
-
 @endsection
