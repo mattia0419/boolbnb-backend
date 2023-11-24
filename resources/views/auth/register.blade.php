fd4c3edf9765092d9bde932b7f342d493c5ef2f0
umbertofaenza <umberto.faenza@gmail.com>
2023-11-24 18:09:57 +0100

    add client-side validation to register form

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -8,19 +8,21 @@
                     <div class="card-header">{{ __('Register') }}</div>
 
                     <div class="card-body">
-                        <form method="POST" action="{{ route('register') }}">
+                        <form method="POST" action="{{ route('register') }}" name="registration">
                             @csrf
 
                             <div class="mb-4 row">
                                 <h6 class="mb-4" style="font-style: italic">Fields with * are required</h6>
 
                                 <label for="first_name"
-                                    class="col-md-4 col-form-label text-md-right">{{ __('First name') }}</label>
+                                    class="col-md-4 col-form-label text-md-right">{{ __('First name *') }}</label>
 
                                 <div class="col-md-6">
                                     <input id="first_name" type="text"
                                         class="form-control @error('first_name') is-invalid @enderror" name="first_name"
                                         value="{{ old('first_name') }}" autocomplete="first_name" autofocus>
+                                        <span class="text-danger" id="first_name_error"></span>
+
 
                                     @error('first_name')
                                         <span class="invalid-feedback" role="alert">
@@ -31,12 +33,14 @@
                             </div>
                             <div class="mb-4 row">
                                 <label for="last_name"
-                                    class="col-md-4 col-form-label text-md-right">{{ __('Last name') }}</label>
+                                    class="col-md-4 col-form-label text-md-right">{{ __('Last name *') }}</label>
 
                                 <div class="col-md-6">
                                     <input id="last_name" type="text"
                                         class="form-control @error('last_name') is-invalid @enderror" name="last_name"
                                         value="{{ old('last_name') }}" autocomplete="last_name" autofocus>
+                                        <span class="text-danger" id="last_name_error"></span>
+
 
                                     @error('last_name')
                                         <span class="invalid-feedback" role="alert">
@@ -71,6 +75,8 @@
                                     <input id="email" type="email"
                                         class="form-control @error('email') is-invalid @enderror" name="email"
                                         value="{{ old('email') }}" required autocomplete="email">
+                                        <span class="text-danger" id="email_error"></span>
+
 
                                     @error('email')
                                         <span class="invalid-feedback" role="alert">
@@ -88,6 +94,7 @@
                                     <input id="password" type="password"
                                         class="form-control @error('password') is-invalid @enderror" name="password"
                                         required autocomplete="new-password">
+                                        <span class="text-danger" id="password_error"></span>
 
                                     @error('password')
                                         <span class="invalid-feedback" role="alert">
@@ -104,6 +111,8 @@
                                 <div class="col-md-6">
                                     <input id="password-confirm" type="password" class="form-control"
                                         name="password_confirmation" required autocomplete="new-password">
+                                        <span class="text-danger" id="confirmation_error"></span>
+
                                 </div>
                             </div>
                             <div class="mb-4 row">
@@ -123,9 +132,9 @@
 
                             <div class="mb-4 row mb-0">
                                 <div class="col-md-6 offset-md-4">
-                                    <button type="submit" class="btn btn-primary">
+                                    <div class="btn btn-primary" onclick="validate()">
                                         {{ __('Register') }}
-                                    </button>
+                                    </div>
                                 </div>
                             </div>
                         </form>
@@ -134,4 +143,65 @@
             </div>
         </div>
     </div>
+@endsection
+
+@section('scripts')
+
+<script type="text/javascript">
+
+    function validate() {
+       let password = document.getElementById('password').value;
+       let confirmation = document.getElementById('password-confirm').value;
+       let firstName = document.getElementById('first_name').value;
+       let lastName = document.getElementById('last_name').value;
+       let email = document.getElementById('email').value;
+
+
+       let passwordError = document.getElementById('password_error');
+       let confirmationError = document.getElementById('confirmation_error');
+       let firstNameError = document.getElementById('first_name_error');
+       let lastNameError = document.getElementById('last_name_error');
+       let emailError = document.getElementById('email_error');
+
+       firstNameError.innerHTML = "";
+       lastNameError.innerHTML = "";
+       emailError.innerHTML = "";
+       passwordError.innerHTML = "";
+
+        if (firstName.length == 0 || lastName.length == 0 || email.length == 0 || password.length == 0 || password != confirmation) {
+
+            if (firstName.length == 0) {
+                firstNameError.innerHTML = "You need to enter a first name";
+            }
+     
+             if (lastName.length == 0) {
+                lastNameError.innerHTML = "You need to enter a last name";
+            }
+     
+             if (email.length == 0) {
+                emailError.innerHTML = "You need to enter an email address";
+            }
+     
+            if (password.length == 0) {
+             passwordError.innerHTML = "You need to enter a password";
+            } else if (password.length < 8 && password.length >= 1) {
+             passwordError.innerHTML = "You must enter at least 8 characters";
+            }
+     
+            if (password != confirmation) {
+                passwordError.innerHTML = "The passwords don't match";
+                confirmationError.innerHTML = "The passwords don't match";
+                document.registration.confirmation.value = "";
+                document.registration.confirmation.focus();
+            }
+
+            return false;
+        }
+
+        if (password == confirmation) {
+          document.registration.submit();
+        }
+    }
+    </script>
+
 @endsection
