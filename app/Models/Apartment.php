ace88fe773c80c27d809549ac6cc32034372ce5d
umbertofaenza <umberto.faenza@gmail.com>
2023-11-27 09:51:27 +0100

    removed unnecessary function from model

--- app/Models/Apartment.php
+++ app/Models/Apartment.php
@@ -35,21 +35,4 @@ class Apartment extends Model
     {
         return $this->belongsToMany(Service::class);
     }
-
-    // public static function getCoordinatesFromAddress(string $address)
-    // {
-    //     $client = new Client();
-    //     $_address = urlencode($address);
-
-    //     $response = $client->get('https://api.tomtom.com/search/2/geocode/' . $_address . '.json', [
-    //         'query' => [
-    //             'key' => 'EoW1gArKxlBBEKl68AZm1uhfhcLougV4',
-    //         ],
-    //     ]);
-    //     error_log(print_r($response, true));
-    //     $data = json_decode($response->getBody(), true);
-    //     $latitude = $data['results'][0]['position']['lat'];
-    //     $longitude = $data['results'][0]['position']['lon'];
-    //     return compact('latitude', 'longitude');
-    // }
 }
