7339f21567c762378d895c2a158c358e11c5f213
umbertofaenza <umberto.faenza@gmail.com>
2023-12-04 11:37:44 +0100

    edit pool icon in service seeder

--- database/seeders/ServiceSeeder.php
+++ database/seeders/ServiceSeeder.php
@@ -8,52 +8,52 @@ use Illuminate\Database\Seeder;
 
 class ServiceSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        $_services = [
+  /**
+   * Run the database seeds.
+   *
+   * @return void
+   */
+  public function run()
+  {
+    $_services = [
 
-            [
-              "label"=> "Cucina",
-              "icon"=> "fa-solid fa-utensils",
-              "id"=> "1",
-            ],
-            [
-              "label"=> "Wi-fi",
-              "icon"=> "fa-solid fa-wifi",
-              "id"=> "2",
-            ],
-            [
-              "label"=> "Posto Macchina",
-              "icon"=> "fa-solid fa-car",
-              "id"=> "3",
-            ],
-            [
-              "label"=> "Piscina",
-              "icon"=> "fa-solid fa-ladder",
-              "id"=> "4",
-            ],
-            [
-              "label"=> "Sauna",
-              "icon"=> "fa-solid fa-bath",
-              "id"=> "5",
-            ],
-            [
-              "label"=> "Vista Mare",
-              "icon"=> "fa-solid fa-water",
-              "id"=> "6",
-            ],
-          ];
-        foreach ($_services as $_service) {
-            $service = new Service;
-            $service->label = $_service["label"];
-            $service->icon = $_service["icon"];
-            $service->id = $_service["id"];
-            $service->save();
+      [
+        "label" => "Cucina",
+        "icon" => "fa-solid fa-utensils",
+        "id" => "1",
+      ],
+      [
+        "label" => "Wi-fi",
+        "icon" => "fa-solid fa-wifi",
+        "id" => "2",
+      ],
+      [
+        "label" => "Posto Macchina",
+        "icon" => "fa-solid fa-car",
+        "id" => "3",
+      ],
+      [
+        "label" => "Piscina",
+        "icon" => "fa-solid fa-water-ladder",
+        "id" => "4",
+      ],
+      [
+        "label" => "Sauna",
+        "icon" => "fa-solid fa-bath",
+        "id" => "5",
+      ],
+      [
+        "label" => "Vista Mare",
+        "icon" => "fa-solid fa-water",
+        "id" => "6",
+      ],
+    ];
+    foreach ($_services as $_service) {
+      $service = new Service;
+      $service->label = $_service["label"];
+      $service->icon = $_service["icon"];
+      $service->id = $_service["id"];
+      $service->save();
     }
-}
+  }
 }
