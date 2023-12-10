cd3b7b831250df896f455dbee190275cea0b1442
umbertofaenza <umberto.faenza@gmail.com>
2023-12-10 21:33:59 +0100

    add messages table

--- resources/views/admin/messages/index.blade.php
+++ resources/views/admin/messages/index.blade.php
@@ -9,18 +9,34 @@
         $messagesExist = false;
     @endphp
 
+<table class="table table-striped">
+    <thead>
+      <tr>
+        <th scope="col">Apartment</th>
+        <th scope="col">Message</th>
+        <th scope="col">From</th>
+        <th scope="col">Received</th>
+      </tr>
+    </thead>
+    <tbody>
     @foreach($messages as $message)
-        @foreach($apartments as $apartment)
-            @if($message->apartment_id == $apartment->id && $apartment->user_id == $user->id)
-                @php
-                    $messagesExist = true;
-                @endphp
-                <h4>{{$apartment->title}}</h4>
-                <span>Email: {{$message->email}}</span>
-                <p>Message: {{$message->text}}</p>
-            @endif
-        @endforeach
-    @endforeach
+    @foreach($apartments as $apartment)
+    @if($message->apartment_id == $apartment->id && $apartment->user_id == $user->id)
+    @php
+        $messagesExist = true;
+    @endphp
+      <tr>
+        <td>
+            <i>{{$apartment->title}}</i></td>
+        <td>{{$message->text}}</td>
+        <td>{{$message->email}}</td>
+        <td>{{$message->created_at}}</td>
+      </tr>
+      @endif
+      @endforeach
+      @endforeach
+    </tbody>
+  </table>
 
     @if(!$messagesExist)
         <h4>No messages</h4>
